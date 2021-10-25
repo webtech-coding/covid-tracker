d8958426f20c2d2db9f64f17cc98715f2bb12f0c
shovit <webtech.delver@gmail.com>
2021-10-26 00:12:03 +0300

    covid tracker app initiated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useMachine } from "@xstate/react";
+
+const App =()=>{
+  //const [current, send] = useMachine()
+  return <div>
+   the covid traker
+  </div>
+}
+
+export default App
